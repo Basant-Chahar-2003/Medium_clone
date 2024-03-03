205e22265ecdf4ac2809c72e5498b0415008a688
basant <basantchahar1707@gmail.com>
2024-03-03 14:24:13 +0530

    changed name in auth.jsx

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,12 +1,12 @@
 import { ChangeEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { SignupInput } from "@hannychahar/common-app";
+import { SignupType } from "@hannychahar/common-app";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
 
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
     const navigate = useNavigate();
-    const [postInputs, setPostInputs] = useState<SignupInput>({
+    const [postInputs, setPostInputs] = useState<SignupType>({
         name: "",
         email: "",
         password: ""
